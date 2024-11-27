312c7bcc25fe4fb5c4e38189473c1f5a23f839ea
chantalzhang <147449578+chantalzhang@users.noreply.github.com>
2024-11-26 22:44:48 -0500

    finished play and test script, renamed minimax alpha beta pruning file

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,7 +2,7 @@ from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .gpt_greedy_corners_agent import StudentAgent
-from .minimax_with_gpt_greedy_corners import Minimax_with_gpt_greedy_corners
+from .minimax_ab import Minimax_with_alpha_beta_pruning
 from .chantal_test_agent import TestAgent
 from .minimax import MiniMax
 from .test_agent import TestAgent
